f0d9581c7da95d373b516280b32b338342592507
Your Name <your_email@gmail.com>
2026-03-31 11:56:04 +0530

    change hello.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Java DevOps Project 🚀";
+        return "Hello from Java DevOps Project !!!!";
     }
 
     @GetMapping("/health")
